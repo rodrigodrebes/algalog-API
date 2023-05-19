9863a097f07ebca63d1ed5127b2a3d0e5424ae5d
Rodrigo <rodrigodrebes97@gmail.com>
2023-05-19 10:09:08 -0300

    "client, controller"

--- src/main/java/com/techsolutions/api/Controller/ClienteController.java
+++ src/main/java/com/techsolutions/api/Controller/ClienteController.java
@@ -1,13 +1,26 @@
 package com.techsolutions.api.Controller;
-
+import java.util.Arrays;
+import java.util.List;
+import com.techsolutions.domain.Model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
-
     @GetMapping("/clientes")
-    public String listar(){
-        return "Teste";
+    public List<Cliente> listar(){
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("João");
+        cliente1.setTelefone("51 99999-99999");
+        cliente1.setEmail("joao@gmail.com");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("Pedro");
+        cliente2.setTelefone("51 88888-99999");
+        cliente2.setEmail("pedro@gmail.com");
+
+        return Arrays.asList(cliente1,cliente2);
     }
 }
--- src/main/java/com/techsolutions/domain/Model/Cliente.java
+++ src/main/java/com/techsolutions/domain/Model/Cliente.java
@@ -1,6 +1,9 @@
 package com.techsolutions.domain.Model;
 
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class Cliente {
     private Long id;
     private String nome;
